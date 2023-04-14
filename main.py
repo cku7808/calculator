e309b8d3625aa727d7836f713f87fc0149ef6199
GWC <cku7808@naver.com>
2023-04-14 15:41:34 +0900

    add te1 and modify btn1 in main.py

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1=QPushButton('Message',self) # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
+        
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
         vbox.addStretch(1) # 빈 공간
         vbox.addWidget(self.btn1) # 버튼 위치
         vbox.addStretch(1) # 빈 공간
         self.setLayout(vbox) # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 설정
+        
         self.setWindowTitle('Calculator')
         self.setWindowIcon(QIcon('icon.png')) # 윈도우 아이콘 추가
         self.resize(256,256)
         self.show()
+        
     def activateMessage(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
-        QMessageBox.information(self,"information","Button clicked!")
+        # QMessageBox.information(self,"information","Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
