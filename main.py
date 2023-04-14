b8f2a9a72222791589dfabf2497478a52716d96f
GWC <cku7808@naver.com>
2023-04-14 15:47:38 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-QMessageBox)
+QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -16,12 +16,23 @@ class Calculator(QWidget):
         self.btn1=QPushButton('Message',self) # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
         
-        vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
-        vbox.addStretch(1) # 빈 공간
-        vbox.addWidget(self.btn1) # 버튼 위치
-        vbox.addStretch(1) # 빈 공간
-        self.setLayout(vbox) # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 설정
+        self.btn2 = QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
         
+        hbox = QHBoxLayout() # 수평박스 레이아웃을 추가하고 버튼 1, 2 추가
+        hbox.addStretch(1) # 공백
+        hbox.addWidget(self.btn1) # 버튼1 배치
+        hbox.addWidget(self.btn2) # 버튼2 배치
+
+        
+        vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
+        vbox.addStretch(self.te1) # 빈 공간
+        # vbox.addWidget(self.btn1) # 버튼 위치
+        # vbox.addStretch(1) # 빈 공간
+        vbox.addLayout(hbox) # 빈공간 - 버튼 - 빈공간 순으로 수직 배치된 레이아웃 설정
+        
+        
+        self.setLayout(vbox)
         self.setWindowTitle('Calculator')
         self.setWindowIcon(QIcon('icon.png')) # 윈도우 아이콘 추가
         self.resize(256,256)
@@ -31,6 +42,8 @@ class Calculator(QWidget):
         # QMessageBox.information(self,"information","Button clicked!")
         self.te1.appendPlainText("Button clicked!")
 
+    def clearMessage(self):
+        self.te1.clear()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
